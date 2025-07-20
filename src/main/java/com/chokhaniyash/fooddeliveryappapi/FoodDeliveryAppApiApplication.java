9b999eee226a567f02b9018e8e1ba4109986e219
Yash Chokhani <yashchokhani95@gmail.com>
2025-07-20 18:40:15 +0530

    project setup

---
+++ src/main/java/com/chokhaniyash/fooddeliveryappapi/FoodDeliveryAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.chokhaniyash.fooddeliveryappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryAppApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chokhaniyash/fooddeliveryappapi/FoodDeliveryAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chokhaniyash.fooddeliveryappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
